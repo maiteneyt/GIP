bb397148fe49cde36bcf846a12f532cff3530c2c
Neyt Maité <maite.neyt>
2017-11-30 14:46:24 +0100

    Add project files.

---
+++ Vliegtuigmaatschappij/test.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace Vliegtuigmaatschappij
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string strConnectie = ConfigurationManager.ConnectionStrings["gip"].ConnectionString;
+
+            OleDbConnection cnn = new OleDbConnection(strConnectie);
+
+            cnn.Open();
+
+            cnn.Close();
+
+        }
+    }
+}
