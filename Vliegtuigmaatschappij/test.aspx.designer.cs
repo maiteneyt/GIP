1a11242987eae9f21304dc03b06195acb122450f
Maite Neyt <maite.neyt@imb-brugge.net>
2017-11-30 15:26:55 +0100

    aanpassing

--- Vliegtuigmaatschappij/test.aspx.designer.cs
+++ Vliegtuigmaatschappij/test.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace Vliegtuigmaatschappij {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Button1 control.
+        /// knopje control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button knopje;
     }
 }
