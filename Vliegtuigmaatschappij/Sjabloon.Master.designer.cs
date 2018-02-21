73e0bed62da927abb177cc81054e761e9f1f12fd
Maite Neyt <maite.neyt@imb-brugge.net>
2018-02-21 11:16:00 +0100

    21/02/2018

--- Vliegtuigmaatschappij/Sjabloon.Master.designer.cs
+++ Vliegtuigmaatschappij/Sjabloon.Master.designer.cs
@@ -31,6 +31,15 @@ namespace Vliegtuigmaatschappij {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
@@ -38,5 +47,14 @@ namespace Vliegtuigmaatschappij {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
     }
 }
---
+++ Vliegtuigmaatschappij/Startpagina.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vliegtuigmaatschappij
+{
+    public partial class Begin_pagina_flight_organisation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //if (Session["Functie"].ToString()== "FlightOrganisation")
+            {
+                //hier moet je de hyperlink zichtbaar maken. 
+            
+            }
+
+        }
+    }
+}
